8b60fb9c494625198819f5ac6f0b3c65eff3de64
PONKATHIRAVAN <ponkathiravanselvam@outlook.com>
2026-04-14 15:22:06 +0530

    Update Program.cs

--- Modern Bakery/Program.cs
+++ Modern Bakery/Program.cs
@@ -5,20 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
  options.UseMySql(
      builder.Configuration.GetConnectionString("DefaultConnection"),
      new MySqlServerVersion(new Version(8, 0, 26))
-     ));
+ ));
 
 builder.Services.AddSession();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,11 +29,12 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
 
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+// 🔥 IMPORTANT FIX FOR RENDER
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+app.Run($"http://0.0.0.0:{port}");
